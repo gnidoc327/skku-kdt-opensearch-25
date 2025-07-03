10fafab6a243f18f71f13184bc13ec2300380b33
김한성 <gnidoc327@gmail.com>
2025-07-04 03:27:15 +0900

    Rename tf resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,7 @@ data "aws_caller_identity" "current" {}
 # 1. 암호화 정책: 데이터가 저장될 때 어떻게 암호화할지 정의합니다.
 #    (AWS 소유 키를 사용한 기본 암호화 설정)
 resource "aws_opensearchserverless_security_policy" "encryption_policy" {
-  name   = "encryption-policy-${var.student_id}"
+  name   = "${var.student_id}-policy"
   type   = "encryption"
   policy = jsonencode({
     "Rules" : [
@@ -52,7 +52,7 @@ resource "aws_opensearchserverless_security_policy" "encryption_policy" {
 # 2. 네트워크 정책: 누가, 어떻게 이 컬렉션에 접근할 수 있는지 네트워크 레벨에서 정의합니다.
 #    (주의: 교육 목적으로 대시보드에 'Public' 접근을 허용합니다.)
 resource "aws_opensearchserverless_security_policy" "network_policy" {
-  name   = "network-policy-${var.student_id}"
+  name   = "${var.student_id}-policy"
   type   = "network"
   policy = jsonencode([
     {
@@ -82,7 +82,7 @@ resource "aws_opensearchserverless_security_policy" "network_policy" {
 
 # 3. 데이터 접근 정책: 어떤 IAM 사용자/역할이 데이터를 읽고 쓸 수 있는지 권한을 정의합니다.
 resource "aws_opensearchserverless_access_policy" "data_access_policy" {
-  name   = "data-access-policy-${var.student_id}"
+  name   = "${var.student_id}-policy"
   type   = "data"
   policy = jsonencode([
     {
